e18283d2237cf991b4f38acd528304b917003f74
VanTrongDuong <vtduong04@gmail.com>
2024-10-16 11:01:11 +0700

    Add .gitignore to exclude unnecessary files

--- XayDungPhanMemQuanLyQuanCaPhe/QLCF_GUI/Properties/Resources.Designer.cs
+++ XayDungPhanMemQuanLyQuanCaPhe/QLCF_GUI/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PhanMemQuanLyQuanCaPhe.Properties {
+namespace QLCF_GUI.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace PhanMemQuanLyQuanCaPhe.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PhanMemQuanLyQuanCaPhe.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("QLCF_GUI.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
