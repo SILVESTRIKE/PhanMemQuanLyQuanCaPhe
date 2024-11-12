860ecf5b4fca743ba7d00c2d61a9cc9b683d8351
VanTrongDuong <vtduong04@gmail.com>
2024-11-12 22:06:36 +0700

    update DB

--- QLCF.sql
+++ QLCF.sql
@@ -1,4 +1,5 @@
-﻿
+﻿USE QLCF
+GO
 
 -- Bảng Mon (Món)
 CREATE TABLE Mon (
@@ -11,7 +12,8 @@ CREATE TABLE Mon (
 CREATE TABLE NguyenLieu (
     IDNguyenLieu CHAR(10) PRIMARY KEY,
     TenNL NVARCHAR(100),
-    SLTon INT
+    SLTon INT,
+	DVTinh NVARCHAR(10)
 );
 
 -- Bảng CongThuc (Công Thức)
@@ -81,30 +83,83 @@ CREATE TABLE ChiTietLichTruc (
     FOREIGN KEY (IDLichTruc) REFERENCES LichTruc(IDLichTruc),
     FOREIGN KEY (IDNhanVien) REFERENCES NhanVien(IDNhanVien)
 );
+INSERT INTO NguyenLieu (IDNguyenLieu, TenNL, SLTon, DVTinh) VALUES
+('NL001', N'Cà Phê', 50, N'kg'),
+('NL002', N'Sữa Tươi', 200, N'L'),
+('NL003', N'Đường', 50, N'kg'),
+('NL004', N'Sốt Socola', 20, N'kg'),
+('NL005', N'Bột Matcha', 10, N'kg'),
+('NL006', N'Bạc Hà', 5, N'kg'),
+('NL007', N'Trá Đào', 100, N'L'),
+('NL008', N'Bột Milo', 15, N'kg'),
+('NL009', N'Trá Xanh', 10, N'kg'),
+('NL010', N'Đào Miếng', 30, N'kg'),
+('NL011', N'Chanh', 300, N'quả'),
+('NL012', N'Cam', 200, N'quả'),
+('NL013', N'Cà Rốt', 100, N'quả'),
+('NL014', N'Trá Đen', 15, N'kg'),
+('NL015', N'Hạt Trân Châu', 50, N'kg'),
+('NL016', N'Sinh Tố Bơ', 100, N'L'),
+('NL017', N'Sinh Tố Dâu', 100, N'L'),
+('NL018', N'Sinh Tố Chuối', 100, N'L'),
+('NL019', N'Bột Cacao', 25, N'kg'),
+('NL020', N'Mật Ong', 30, N'L');
 
--- Nhập dữ liệu vào bảng Mon (Món)
-INSERT INTO Mon (IDMon, TenMon, price) VALUES
-('M001', 'Cà phê đen', 25000),
-('M002', 'Cà phê sữa', 30000),
-('M003', 'Trà đào', 35000),
-('M004', 'Nước cam', 40000),
-('M005', 'Sinh tố xoài', 45000);
+INSERT INTO Mon (IDMon, TenMon, Gia) VALUES
+('M001', N'Cà Phê Đen', 20000),
+('M002', N'Cà Phê Sữa', 25000),
+('M003', N'Cà Phê Đá Xay', 40000),
+('M004', N'Trá Đào', 30000),
+('M005', N'Trá Sữa Trân Châu', 35000),
+('M006', N'Matcha Latte', 45000),
+('M007', N'Sinh Tố Bơ', 50000),
+('M008', N'Sinh Tố Dâu', 50000),
+('M009', N'Nước Cam', 30000),
+('M010', N'Nước Chanh', 25000),
+('M011', N'Sữa Tươi Trân Châu Đường Đen', 40000),
+('M012', N'Trá Sữa Matcha', 45000),
+('M013', N'Soda Bạc Hà', 30000),
+('M014', N'Milo Đá Xay', 35000),
+('M015', N'Cà Phê Cốt Dừa', 40000),
+('M016', N'Sinh Tố Chuối', 50000),
+('M017', N'Trá Sữa Truyền Thống', 35000),
+('M018', N'Sinh Tố Cam Cà Rốt', 50000),
+('M019', N'Trá Sữa Hồng Trà', 40000),
+('M020', N'Trá Chanh Mật Ong', 30000);
 
--- Nhập dữ liệu vào bảng NguyenLieu (Nguyên Liệu)
-INSERT INTO NguyenLieu (IDNguyenLieu, TenNL, SLTon) VALUES
-('NL001', 'Cà phê hạt', 50),
-('NL002', 'Sữa đặc', 30),
-('NL003', 'Đào ngâm', 20),
-('NL004', 'Cam tươi', 40),
-('NL005', 'Xoài chín', 25);
-
--- Nhập dữ liệu vào bảng CongThuc (Công Thức)
 INSERT INTO CongThuc (IDMon, IDNguyenLieu, SoLuong) VALUES
-('M001', 'NL001', 10.0),
-('M002', 'NL001', 8.0),
-('M002', 'NL002', 2.0),
-('M003', 'NL003', 5.0),
-('M004', 'NL004', 4.0),
-('M005', 'NL005', 3.0);
+('M001', 'NL001', 0.02),
+('M002', 'NL001', 0.02),
+('M002', 'NL002', 0.03),
+('M003', 'NL001', 0.02),
+('M003', 'NL004', 0.01),
+('M004', 'NL007', 0.25),
+('M005', 'NL017', 0.05),
+('M005', 'NL002', 0.1),
+('M006', 'NL005', 0.02),
+('M006', 'NL002', 0.15),
+('M007', 'NL016', 0.2),
+('M008', 'NL018', 0.2),
+('M009', 'NL014', 0.3),
+('M010', 'NL013', 0.2),
+('M011', 'NL015', 0.05),
+('M011', 'NL002', 0.15),
+('M012', 'NL009', 0.02),
+('M012', 'NL002', 0.1),
+('M013', 'NL006', 0.05),
+('M014', 'NL008', 0.02),
+('M015', 'NL001', 0.02),
+('M015', 'NL010', 0.05),
+('M016', 'NL019', 0.1),
+('M017', 'NL002', 0.1),
+('M018', 'NL012', 0.15),
+('M018', 'NL011', 0.1),
+('M019', 'NL014', 0.15),
+('M020', 'NL020', 0.02);
 
-
+INSERT INTO NhanVien (IDNhanVien, Ten, SDT, GTinh, NgSinh, TrangThai, IDQuanLy, Pass) VALUES
+('NV001', N'Trần Văn A', '0900000001', N'Nam', '1990-05-15', 1, 'NV001', '123');
+--('NV002', N'Nguyễn Thị B', '0900000002', N'Nữ', '1992-08-22', 1, 'NV001', 'pass123'),
+--('NV003', N'Lê Văn C', '0900000003', N'Nam', '1988-03-09', 1, 'NV001', 'pass123'),
+--('NV004', N'Pham Thi D', '0900000004', N'Nữ', '1995-12-30', 1, 'NV002', 'pass123'),
+--('NV005', N'Nguyen Van E', '0900000005', N'Nam', '1991-07-21', 1, 'NV002', 'pass123');
